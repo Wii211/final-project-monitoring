e594f3582cc2c5ed650effed8918c42a93c307d1
Irham <irhammaulani@gmail.com>
2019-10-29 02:37:51 +0800

    change schedule to deadline_schedules

--- database/migrations/2019_10_24_140339_create_deadline_schedules_table.php
+++ database/migrations/2019_10_24_140339_create_deadline_schedules_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSchedulesTable extends Migration
+class CreateDeadlineSchedulesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateSchedulesTable extends Migration
      */
     public function up()
     {
-        Schema::create('schedules', function (Blueprint $table) {
+        Schema::create('deadline_schedules', function (Blueprint $table) {
             $table->increments('id');
             $table->dateTime('start_date')->nullable();
             $table->dateTime('end_date')->nullable();
