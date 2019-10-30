0cc7d717781b9ef71952789f9d3a1065ff11e1d6
Irham <irhammaulani@gmail.com>
2019-10-31 03:02:26 +0800

    logout

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::view('/', 'welcome');
+Route::view('/home', 'welcome');
 
 // :(((())))
 Route::prefix('student')->group(function () {
@@ -44,6 +44,7 @@ Route::prefix('user')->group(function () {
     Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login.index');
 
     Route::post('/login', 'Auth\LoginController@login')->name('login.store');
+    Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
 });
 
 Route::view('/final_project', 'final_projects.datas')->name('final.datas');
