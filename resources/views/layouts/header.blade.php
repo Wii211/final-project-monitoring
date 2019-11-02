919d5a7b5e7b86c229b233dd74f6498a35a29dbe
addid10 <rezkynewaditya@gmail.com>
2019-11-02 21:06:23 +0800

    add crsf token

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,6 +8,8 @@
     </title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css') }}">
         @yield('galery')
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
@@ -19,5 +21,6 @@
     <link rel="stylesheet" href="{{ asset('assets/plugins/daterangepicker/daterangepicker.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/plugins/summernote/summernote-bs4.css') }}">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+        @yield('css')
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
 </head>
