d9935dc9eb4e79c5f9b7632a717c7f551aef4a08
Irham <irhammaulani@gmail.com>
2019-11-03 00:33:59 +0800

    add LecturerTopicFactory and add seeder in databaseSeeder

--- database/factories/LecturerTopicFactory.php
+++ database/factories/LecturerTopicFactory.php
@@ -3,7 +3,13 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\LecturerTopic::class, function (Faker $faker) {
+
+    $lecturerId = App\Lecturer::pluck('id')->toArray();
+
+    $topicId = App\Topic::pluck('id')->toArray();
+
     return [
-        //
+        'lecturer_id' => $faker->randomElement($lecturerId),
+        'topic_id' => $faker->randomElement($topicId),
     ];
 });
