e80afb31831c78938fb2a550ac715711992874be
addid10 <rezkynewaditya@gmail.com>
2019-11-04 13:57:59 +0800

    add sweetalert for delete in lecturer datas

--- public/assets/js/lecturer/lecturer.js
+++ public/assets/js/lecturer/lecturer.js
@@ -235,3 +235,30 @@ $(document).on('submit', '#lecturerDataForm', function (e) {
         });
     }
 });
+
+
+//Delete
+$('#lecturerTable tbody').on('click', '.delete', function () {
+    let id = $(this).attr("id");
+
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.value) {
+            Swal.fire(
+                    'Deleted! Id' + id,
+                    'Your file has been deleted.',
+                    'success'
+                )
+                .then(function () {
+                    dataTable.ajax.reload();
+                });
+        }
+    })
+});
