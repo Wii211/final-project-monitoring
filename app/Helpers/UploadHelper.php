8005b1484e645ab5f36ec63ecd6f1f76af2584b9
Irham <irhammaulani@gmail.com>
2019-11-06 04:12:52 +0800

    add upload File

--- app/Helpers/UploadHelper.php
+++ app/Helpers/UploadHelper.php
@@ -39,4 +39,18 @@ class UploadHelper
             return true;
         }
     }
+
+    public function uploadFile(UploadedFile $uploadedFile, $idName, $folder)
+    {
+        $timeStamp = Carbon::now()->timestamp;
+
+        $fileName = !is_null($idName) ? $idName . '_' .
+            $timeStamp . '_' . uniqId() : str_random(25);
+
+        if ($uploadedFile->storeAs('public/' . $folder, $fileName)) {
+            return $folder . '/' . $fileName;
+        } else {
+            return false;
+        }
+    }
 }
