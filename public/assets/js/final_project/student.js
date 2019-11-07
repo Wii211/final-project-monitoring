e52fc5478f3d6d358352bada36e95f0466f49739
addid10 <rezkynewaditya@gmail.com>
2019-11-07 20:56:33 +0800

    fix ajax method in student.js

--- public/assets/js/final_project/student.js
+++ public/assets/js/final_project/student.js
@@ -21,9 +21,9 @@ let dataTable = $('#finalStudentTable').DataTable({
                 let status = full.is_verified;
                 let buttonId = full.id;
                 if (status == 0) {
-                    return "<button class='btn btn-success verification' id='" + buttonId + "'>Verification</button>";
+                    return "<button class='btn btn-success verification' id='" + buttonId + "' value='" + status + "'>Verification</button>";
                 } else {
-                    return "<button class='btn btn-danger verification' id='" + buttonId + "'>Unverification</button>";
+                    return "<button class='btn btn-danger verification' id='" + buttonId + "' value='" + status + "'>Unverification</button>";
                 }
             }
         }
@@ -34,6 +34,14 @@ let dataTable = $('#finalStudentTable').DataTable({
 //Update
 $('#finalStudentTable tbody').on('click', '.verification', function () {
     let id = $(this).attr("id");
+    let status = $(this).val();
+    let method;
+
+    if (status == 1) {
+        method = "DELETE";
+    } else if (status == 0) {
+        method = "PUT";
+    }
 
     Swal.fire({
         title: 'Are you sure?',
@@ -49,7 +57,7 @@ $('#finalStudentTable tbody').on('click', '.verification', function () {
                 url: "students/" + id,
                 type: "POST",
                 data: {
-                    "_method": "PUT"
+                    "_method": method
                 },
                 success: function () {
                     Swal.fire(
