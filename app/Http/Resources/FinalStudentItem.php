31ad64b6eb58587bd7dcd417f204e6085bf922c4
Irham <irhammaulani@gmail.com>
2019-11-07 19:45:49 +0800

    change to arraytoParent

--- app/Http/Resources/FinalStudentItem.php
+++ app/Http/Resources/FinalStudentItem.php
@@ -16,18 +16,6 @@ class FinalStudentItem extends JsonResource
     public function toArray($request)
     {
 
-        return [
-            'id'    => $this->id,
-            'transcript' => $this->transcript,
-            'student_id' => $this->student_id,
-            'name' => $this->name,
-            'status' => $this->status,
-            'is_verified' => $this->is_verified,
-            'latest_study_plan' => $this->latest_study_plan,
-            'user_id' => $this->user_id,
-            'user' => new UserItem($this->whenLoaded('user')),
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-        ];
+        return parent::toArray($request);
     }
 }
