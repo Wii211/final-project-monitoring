446e69c003be41514201bb0c2a3bfc35317d91af
addid10 <rezkynewaditya@gmail.com>
2019-11-08 16:35:28 +0800

    add new modal

---
+++ resources/views/modals/final_project/recommendation.blade.php
@@ -0,0 +1,48 @@
+<div class="modal fade" id="recomendationTitle" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="">Rekomendasi Judul</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="">Judul</label>
+                        <input type="text" class="form-control" id="" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Keterangan Topik</label>
+                        <textarea class="form-control" id="" rows="3" required></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Topik</label>
+                        <select multiple class="form-control" id="">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Dosen Pengampu</label>
+                        <select class="form-control" id="">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Tambah</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
