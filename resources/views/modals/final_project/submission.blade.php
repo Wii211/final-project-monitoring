583fed3de8d76e88ea30fcce8d35188c52242914
addid10 <rezkynewaditya@gmail.com>
2019-11-08 16:37:26 +0800

    delete choice of titles from the lecturer

--- resources/views/modals/final_project/submission.blade.php
+++ resources/views/modals/final_project/submission.blade.php
@@ -9,41 +9,13 @@
             </div>
             <form>
                 <div class="modal-body">
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="type" id="studentTitle" value="option1"
-                            checked>
-                        <label class="form-check-label" for="studentTitle">
-                            Judul sendiri
-                        </label>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Judul Proposal</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1">
                     </div>
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="type" id="lecturerTitle" value="option2">
-                        <label class="form-check-label" for="lecturerTitle">
-                            Judul dari dosen
-                        </label>
-                    </div>
-                    <hr>
-                    <div class="lecturer-final-project-title">
-                        <div class="form-group mt-3">
-                            <label for="exampleFormControlSelect2">Judul Proposal Dari Dosen</label>
-                            <select multiple class="form-control" id="exampleFormControlSelect2">
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="student-final-project-title">
-                        <div class="form-group">
-                            <label for="exampleFormControlInput1">Judul Proposal</label>
-                            <input type="text" class="form-control" id="exampleFormControlInput1">
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleFormControlInput1">Deskripsi Umum Proposal Tugas Akhir</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                        </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Deskripsi Umum Proposal Tugas Akhir</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                     </div>
                     <div class="form-group">
                         <label for="exampleFormControlSelect1">Dosen Pembimbing Pertama</label>
@@ -65,6 +37,8 @@
                             <option>5</option>
                         </select>
                     </div>
+                    <hr>
+                    <a href="{{ route('recomendation-title.index') }}" class="btn bg-gradient-primary w-100">Klik di sini untuk memilih judul dari dosen, tersedia topik baru!</a>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
