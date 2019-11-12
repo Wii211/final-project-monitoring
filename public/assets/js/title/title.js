ea47c015bf80f81659813b6845d6ddf0321ff84e
addid10 <rezkynewaditya@gmail.com>
2019-11-12 15:57:16 +0800

    create new js for title page

---
+++ public/assets/js/title/title.js
@@ -0,0 +1,21 @@
+// $('#title-table tbody').on('submit', '#field-title', function (e) {
+//     e.preventDefault();
+//     let role = 1;
+//     let lecturer = $('#lecturerId').val();
+
+//     let supervisors = {
+//         'lecturer': lecturer,
+//         'role': role
+//     };
+//     let formData = new FormData(this);
+
+//     formData.append('supervisors', supervisors);
+
+//     $.ajax({
+//         type: "POST",
+//         data: formData,
+//         contentType: false,
+//         processData: false
+//     })
+
+// })
