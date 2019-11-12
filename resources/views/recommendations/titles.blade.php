778625f6962ac07bf352c4d17076b7ea9b490084
addid10 <rezkynewaditya@gmail.com>
2019-11-12 14:43:44 +0800

    validate access users for features

--- resources/views/recommendations/titles.blade.php
+++ resources/views/recommendations/titles.blade.php
@@ -6,6 +6,7 @@
 @section('content')
 <section class="content">
     <div class="container-fluid">
+        @if(Auth::user()->isAdmin())
         <div class="d-flex justify-content-end">
             {{-- <button type="button" class="btn bg-gradient-success mb-2" id="convert">
                 <i class="fas fa-images"></i>
@@ -13,14 +14,14 @@
             <button type="submit" class="btn btn-primary mb-2" data-toggle="modal"
                 data-target="#recomendationTitle">Tambah Rekomendasi Judul</button>
         </div>
+        @endif
         <div class="card">
             <div class="card-body pb-0">
-                <form action="{{route('recomendation-title.index')}}" method="GET">
+                <form class="row" action="{{route('recomendation-title.index')}}" method="GET">
                     <div class="form-group col-md-9">
                         <input type="text" name="q" class="form-control form-control-sm" placeholder="Cari judul..."  id="" required>
                     </div>
                     <div class="form-group col-md-3">
-                    
                         <button type="submit" value="q" class="btn bg-gradient-primary w-100">Cari</button>
                     </div>
                 </form>
@@ -33,9 +34,10 @@
                             <th>Judul</th>
                             <th>Dosen</th>
                             <th width="30%">Keterangan</th>
+                            @if(Auth::user()->isAdmin())
                             <th width="5%"></th>
                             <th width="5%"></th>
-                            <th width="5%"></th>
+                            @endif
                             <th width="5%"></th>
                         </tr>
                     </thead>
@@ -51,6 +53,7 @@
                         <td> {{$title->title}} </td>
                             <td>{{$title->lecturer->name}}</td>
                             <td>{{$title->description}}</td>
+                            @if(Auth::user()->isAdmin())
                             <td>
                                 <button class="btn bg-gradient-warning btn-sm w-100" data-toggle="modal"
                                     data-target="#updateProposal">Update</button>
@@ -59,9 +62,10 @@
                                 <button class="btn bg-gradient-danger btn-sm w-100" data-toggle="modal"
                                     data-target="#updateProposal">Delete</button>
                             </td>
+                            @endif
                             <td>
                                 <button class="btn bg-gradient-success btn-sm w-100" data-toggle="modal"
-                                    data-target="#updateProposal">Ambil Bgst</button>
+                                    data-target="#updateProposal">Ambil</button>
                             </td>
                         </tr>
                         @endforeach
