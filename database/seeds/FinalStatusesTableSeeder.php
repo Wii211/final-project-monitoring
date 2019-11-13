c2b24d51afebef2ac8357a99beac0b5ec925ca5b
Irham <irhammaulani@gmail.com>
2019-11-13 15:31:36 +0800

    add finalStatusesTableSeeder

--- database/seeds/FinalStatusesTableSeeder.php
+++ database/seeds/FinalStatusesTableSeeder.php
@@ -11,6 +11,44 @@ class FinalStatusesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        if (DB::table('final_statuses')->get()->count() == 0) {
+            DB::table('final_statuses')->insert([
+                [
+                    'name' => 'pendaftaran',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'pra-proposal',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'proposal',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'revisi_proposal',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'tugas_akhir',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'revisi_tugas_akhir',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'tugas_akhur_selesai',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ]
+            ]);
+        }
     }
 }
