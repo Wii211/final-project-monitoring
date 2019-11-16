4db5db25163e971239cdafdf440d32bd61d44add
Irham <irhammaulani@gmail.com>
2019-11-16 14:37:17 +0800

    add response json function

--- app/Http/Controllers/LecturerController.php
+++ app/Http/Controllers/LecturerController.php
@@ -23,13 +23,13 @@ class LecturerController extends Controller
      */
     public function index(Request $request)
     {
-
         $q = null;
 
         if ($request->has('primary')) $q = $request->query('primary');
 
-        return $request->ajax() ? $this->lecturerService
-            ->getListData(null, $q) : view('datas.lecturer');
+        return $request->ajax() ? response()->json(
+            $this->lecturerService->getListData(null, $q, null)
+        ) : view('datas.lecturer');
     }
 
     /**
