3ccd07767cf8731c37216cf4b9162c5667f34395
addid10 <rezkynewaditya@gmail.com>
2019-11-19 20:47:50 +0800

    fix frontend

--- resources/views/students/final_project.blade.php
+++ resources/views/students/final_project.blade.php
@@ -12,13 +12,18 @@
                 <table class="table">
                     <thead>
                         <tr>
+                            <th colspan="4">Deskripsi Tugas Akhir</th>
+                        </tr>
+                        <tr>
                             <th>Judul</th>
+                            <th>Tanggal</th>
                             <th>Status</th>
                             <th width="10%"></th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
+                            <td>Update software without click</td>
                             <td>Update software without click</td>
                             <td><span class="badge badge-primary p-2">Tugas Akhir</span></td>
                             <td>
@@ -35,17 +40,20 @@
                 <table class="table table-bordered">
                     <thead>
                         <tr>
+                            <th colspan="2">Dosen Pembimbing</th>
+                        </tr>
+                        <tr>
                             <th>Nama</th>
                             <th>Status</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td>Konsultasi ke Dosen Pembimbing</td>
+                            <td>Pa Darmansyah</td>
                             <td>Pembimbing 1</td>
                         </tr>
                         <tr>
-                            <td>Lampiran 1</td>
+                            <td>Pa Husnul Khatimi</td>
                             <td>Pembimbing 2</td>
                         </tr>
                     </tbody>
@@ -58,7 +66,7 @@
                     <thead>
                         <tr>
                             <th style="width:5%">#</th>
-                            <th>Task</th>
+                            <th>Berkas</th>
                             <th style="width:60%"></th>
                         </tr>
                     </thead>
@@ -83,6 +91,9 @@
                 <table class="table table-bordered">
                     <thead>
                         <tr>
+                            <th colspan="4">Seminar</th>
+                        </tr>
+                        <tr>
                             <th style="width:5%">#</th>
                             <th>Tanggal</th>
                             <th>Waktu</th>
@@ -92,9 +103,9 @@
                     <tbody>
                         <tr>
                             <td>1.</td>
-                            <td>Pedoman Tugas Akhir</td>
-                            <td><a href="#">Download Berkas Lampiran</a> </td>
-                            <td><a href="#">Download Berkas Lampiran</a> </td>
+                            <td>19-19-2019</td>
+                            <td>21:00</td>
+                            <td>Ruangan A-16</td>
                         </tr>
                     </tbody>
                 </table>
@@ -108,4 +119,3 @@
 @include('modals.final_project.progress')
 @include('modals.final_project.detail')
 @endsection
-  
