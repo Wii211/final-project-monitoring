cce522a5071893bf930edb6b84bf2125f780f73f
Irham <irhammaulani@gmail.com>
2019-11-21 14:18:15 +0800

    store and update

--- app/Http/Services/FinalStudentService.php
+++ app/Http/Services/FinalStudentService.php
@@ -3,21 +3,30 @@
 
 namespace App\Http\Services;
 
-use App\Lecturer;
+use App\Role;
 
+use App\User;
+use App\Lecturer;
 use App\FinalStudent;
 use Illuminate\Http\Request;
 use App\Helpers\UploadHelper;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 class FinalStudentService
 {
-    private $finalStudent, $lecturer;
+    private $finalStudent, $lecturer, $user, $role;
 
-    public function __construct(Lecturer $lecturer, FinalStudent $finalStudent)
-    {
+    public function __construct(
+        Lecturer $lecturer,
+        FinalStudent $finalStudent,
+        User $user,
+        Role $role
+    ) {
         $this->lecturer = $lecturer;
         $this->finalStudent = $finalStudent;
+        $this->user = $user;
+        $this->role = $role;
     }
 
     public function getListData($relation = null, $active)
@@ -37,5 +46,65 @@ class FinalStudentService
     }
 
     public function store(Request $request)
-    { }
+    {
+        try {
+            DB::transaction(function () use ($request) {
+
+                $user = $this->user([
+                    'user_name' => $request->student_id,
+                    'email' => $request->email,
+                    'phone_number' => $request->phone_number,
+                    'gender' => $request->gender,
+                    'image_profile' => 'https://lorempixel.com/640/480/?40069',
+                    'password' => $request->student_id . $request->name,
+                ]);
+
+                $user->save();
+
+                $user->roles()->sync($this->role->name('mahasiswa')->id);
+
+                $finalStudent = $this->finalStudent([
+                    'student_id' => $request->student_id,
+                    'status' => 0,
+                    'is_verified' => 0,
+                    'user_id' => $user->id,
+                    'name' => $request->name
+                ]);
+
+                $finalStudent->save();
+            });
+        } catch (\Throwable $th) {
+            return false;
+        }
+        return true;
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        try {
+            DB::transaction(function () use ($request, $id) {
+                $user = $this->user->findOrFail($id);
+
+                $user->user_name = $request->student_id;
+                $user->email = $request->email;
+                $user->phone_number = $request->phone_number;
+                $user->gender = $request->gender;
+                $user->password = $request->password;
+
+                $user->save();
+
+                $finalStudent = $this->finalStudent->whereUserId($id)->first();
+                $finalStudent->name = $request->name;
+                $finalStudent->student_id = $request->student_id;
+                $finalStudent->status = $request->status;
+                $finalStudent->is_verified = $request->is_verified;
+
+                $finalStudent->save();
+            });
+        } catch (\Throwable $th) {
+            return false;
+        }
+        return true;
+    }
 }
