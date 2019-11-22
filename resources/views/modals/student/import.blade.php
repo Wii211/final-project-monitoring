d12bc55d77a78ce521fc100a364be3ee242986dd
addid10 <rezkynewaditya@gmail.com>
2019-11-22 15:11:03 +0800

    change name request on import blade

--- resources/views/modals/student/import.blade.php
+++ resources/views/modals/student/import.blade.php
@@ -1,30 +1,24 @@
 <div class="modal fade" id="import-modal" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="import-title">Import Data Mahasiswa</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <form id="import-form" enctype="multipart/form-data">
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <label for="final-import-id">Import</label>
-                            <input type="file" class="form-control-file" id="import" name="import" required>
-                        </div>
-                        {{-- <div class="form-group mt-3">
-                            <label for="user-id">Session</label>
-                            <select class="form-control form-control-sm" id="user-id" name="user_id" required>
-                            </select>
-                        </div> --}}
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-success" id="import-action">Import</button>
-                    </div>
-                </form>
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="import-title">Import Data Mahasiswa</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
+            <form id="import-form" enctype="multipart/form-data">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="final-import-id">Import</label>
+                        <input type="file" class="form-control-file" id="import" name="file" required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-success" id="import-action">Import</button>
+                </div>
+            </form>
         </div>
     </div>
-    
+</div>
