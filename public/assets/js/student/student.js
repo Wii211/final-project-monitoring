800c33fc5f3a29559c23d9133e892ce34b582ada
addid10 <rezkynewaditya@gmail.com>
2019-11-22 16:39:51 +0800

    delete detail row

--- public/assets/js/student/student.js
+++ public/assets/js/student/student.js
@@ -11,7 +11,8 @@ let dataTable = $('#student-table').DataTable({
     "ajax": {
         url: "students"
     },
-    "columns": [{
+    "columns": [
+        {
             data: 'student_id'
         },
         {
@@ -19,13 +20,6 @@ let dataTable = $('#student-table').DataTable({
         },
         {
             data: 'status'
-        },
-        {
-            sortable: false,
-            "render": function (data, type, full, meta) {
-                let buttonId = full.id;
-                return '<button id="' + buttonId + '" class="btn btn-info detail">Detail</button>';
-            }
         },
         {
             sortable: false,
@@ -47,33 +41,6 @@ let dataTable = $('#student-table').DataTable({
     }]
 });
 
-function indexImages(id) {
-    $.ajax({
-        url: "students-images/" + id,
-        success: function (data) {
-            $('#student-images-modal').modal('show');
-            $('#student-images').html('');
-
-            data.forEach(function (result) {
-                let image = '<div class="col-md-6">' +
-                    '<div class="image-position">' +
-                    '<img src="../storage/' + result.image + '" class="w-100 mb-5 rounded image-detail" alt="' + result.name + '">' +
-                    '<button id="' + result.id + '" class="btn btn-danger btn-sm delete-images" value="' + id + '"><span class="mdi mdi-delete"></span></button>' +
-                    '</div>' +
-                    '</div>';
-
-                $('#student-images').append(image);
-            })
-        }
-    })
-}
-
-// Show Images
-$('#student-table tbody').on('click', '.show-images', function () {
-    let id = $(this).attr('id');
-    indexImages(id)
-});
-
 // Click Button Add
 $('#student-add').click(function () {
     $('#student-form')[0].reset();
@@ -89,6 +56,7 @@ $('#student-table tbody').on('click', '.update', function () {
         url: "students/" + id,
         dataType: "json",
         success: function (result) {
+            console.log(result);
             $('#student-modal').modal('show');
             $('#student-title').text("Update student");
             $('#student-action').text("Update");
@@ -162,86 +130,4 @@ $(document).on('submit', '#student-form', function (e) {
             }
         });
     }
-});
-
-//Delete image
-$(document).on('click', '.delete-images', function () {
-    let id = $(this).attr("id");
-    let charaId = $(this).val()
-
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-        if (result.value) {
-            Swal.fire({
-                title: 'Loading',
-                timer: 60000,
-                onBeforeOpen: () => {
-                    Swal.showLoading()
-                }
-            });
-
-            $.ajax({
-                url: "students-images/" + id,
-                type: 'DELETE',
-                success: function () {
-
-                    Swal.fire(
-                            'Deleted!',
-                            'Images successfully deleted.',
-                            'success'
-                        )
-                        .then(function () {
-                            indexImages(charaId)
-                        });
-                }
-            });
-        }
-    })
-});
-
-// Delete student
-$('#student-table tbody').on('click', '.delete', function () {
-    let id = $(this).attr("id");
-
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-        if (result.value) {
-            Swal.fire({
-                title: 'Loading',
-                timer: 60000,
-                onBeforeOpen: () => {
-                    Swal.showLoading()
-                }
-            });
-
-            $.ajax({
-                url: "students/" + id,
-                type: 'DELETE',
-                success: function () {
-                    Swal.fire(
-                            'Deleted.',
-                            'student successfully deleted!',
-                            'success'
-                        )
-                        .then(function () {
-                            dataTable.ajax.reload();
-                        });
-                }
-            });
-        }
-    })
 });
