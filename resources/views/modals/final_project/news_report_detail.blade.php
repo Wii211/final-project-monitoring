51dfbf9496f0572a931f0bdbca616a6ccbf41a2b
addid10 <rezkynewaditya@gmail.com>
2019-12-10 17:43:57 +0800

    add modal news report detail image

---
+++ resources/views/modals/final_project/news_report_detail.blade.php
@@ -0,0 +1,13 @@
+<div class="modal fade" id="news-report-detail-modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body" id="detail-image">
+            </div>
+        </div>
+    </div>
+</div>
