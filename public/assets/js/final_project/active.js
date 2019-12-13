bd4b0522f33907d1dfd52d04ea947c62c600fb05
addid10 <rezkynewaditya@gmail.com>
2019-12-13 18:24:56 +0800

    update data

--- public/assets/js/final_project/active.js
+++ public/assets/js/final_project/active.js
@@ -2,7 +2,34 @@ $.ajaxSetup({
     headers: {
         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
     }
-});
+})
+
+
+$(document).ready(function () {
+    $.ajax({
+        url: "../supervisor?primary=true",
+        type: "GET",
+        dataType: "json",
+        success: function (lecturer) {
+            lecturer.data.forEach(function (result) {
+                let data = '<option value="' + result.id + '">' + result.name + '</option>';
+                $('#supervisors-1').append(data);
+            });
+        }
+    })
+
+    $.ajax({
+        url: "../supervisor?primary=false",
+        type: "GET",
+        dataType: "json",
+        success: function (lecturer) {
+            lecturer.data.forEach(function (result) {
+                let data = '<option value="' + result.id + '">' + result.name + '</option>';
+                $('#supervisors-2').append(data);
+            });
+        }
+    })
+})
 
 let dataTable = $('#final-project-table').DataTable({
     "processing": true,
@@ -20,7 +47,7 @@ let dataTable = $('#final-project-table').DataTable({
             "render": function (data, type, full, meta) {
                 let status
 
-                full.final_project.final_logs.forEach(function (data){
+                full.final_project.final_logs.forEach(function (data) {
                     console.log(data.final_status.name)
                     status = data.final_status.name
                 })
@@ -31,34 +58,99 @@ let dataTable = $('#final-project-table').DataTable({
         {
             sortable: false,
             "render": function (data, type, full, meta) {
-                let buttonId = full.id;
+                let buttonId = full.final_project.id;
                 return "<button class='btn btn-success verification' id='" + buttonId + "'>Verifikasi</button>";
             }
         },
         {
             sortable: false,
             "render": function (data, type, full, meta) {
-                let buttonId = full.id;
+                let buttonId = full.final_project.id;
                 return "<button class='btn btn-primary progress-agreement' id='" + buttonId + "'>Progress</button>";
             }
         },
         {
             sortable: false,
             "render": function (data, type, full, meta) {
-                let buttonId = full.id;
+                let buttonId = full.final_project.id;
                 return "<button class='btn btn-warning update' id='" + buttonId + "'>Update</button>";
             }
         }
     ]
 })
 
-// Verification
+//Fetch datas for update
+let i = 1;
+$('#final-project-table tbody').on('click', '.update', function () {
+    let id = $(this).attr('id');
+
+    $.ajax({
+        url: "../data/final_project/" + id,
+        dataType: "json",
+        success: function (result) {
+            $('#final-project-active-modal').modal('show')
+            $('#final-project-active-title').text("Update Tugas Akhir")
+            $('#final-project-active-id').val(result.data.id)
+            $('#final-student-active-id').val(result.data.final_student_id)
+            $('#description-active').val(result.data.description)
+
+            $('#title-active').val(result.data.title)
+
+            result.data.supervisors.forEach(function (data) {
+                $('#supervisors-' + i).val(data.lecturer_id)
+                $('#supervisors-role-' + i).val(data.role)
+                i++
+            })
+        }
+    })
+});
+
+function verification(id){
+    let n = 1
+    $.ajax({
+        url: "student-status/" + id,
+        success: function (result) {
+            $('#final-project-verification-modal').modal('show')
+            $('#final-project-verification-id').val(id)
+            $('#final-status-table-row').html('')
+            result.data.forEach(function (result) {
+                let status
+                let button 
+                if (result.is_verification === 1) {
+                    status = '<span class="badge badge-success p-2">Telah diverifikasi</span>'
+                    button = ''
+                } else {
+                    status = '<span class="badge badge-warning p-2">Belum diverifikasi</span>'
+                    button = '<button type="button" class="btn btn-success final-status-check" id="' + result.id + '">' + 
+                    '<i class="fas fa-check"></i></button>'
+                }
+
+                let data = '<tr>' +
+                    '<td>' + n + '</td>' +
+                    '<td>' + result.final_status.name + '</td>' +
+                    '<td>' + status + '</td>' +
+                    '<td>' + button + '</td>' +
+                    '</tr>';
+                n++
+                $('#final-status-table-row').append(data);
+
+            })
+        }
+    })
+}
+
+// Verificationx
 $('#final-project-table tbody').on('click', '.verification', function () {
-    $('#final-project-verification-modal').modal('show')
+    let id = $(this).attr("id");
+
+    verification(id)
 })
 
+
 $('#final-status-table tbody').on('click', '.final-status-check', function () {
-    let id = $(this).attr("id");
+    let id = $(this).attr("id")
+    let finalId = $('#final-project-verification-id').val()
+
     Swal.fire({
         title: 'Are you sure?',
         text: "You won't be able to revert this!",
@@ -69,17 +161,28 @@ $('#final-status-table tbody').on('click', '.final-status-check', function () {
         confirmButtonText: 'Yes, verified it!'
     }).then((result) => {
         if (result.value) {
+            Swal.fire({
+                title: 'Loading',
+                timer: 60000,
+                onBeforeOpen: () => {
+                    Swal.showLoading()
+                }
+            })
             $.ajax({
-                // url: "news-report-image/" + id,
-                // type: 'DELETE',
+                url: "student-status/" + id + "/verify",
+                type: 'POST',
+                data: {
+                    '_method': 'PUT', 
+                    'is_verification': 1
+                }, 
                 success: function () {
                     Swal.fire(
                             'Verified!',
-                            'Telah dihapus!',
+                            'Telah diverifikasi!',
                             'success'
                         )
                         .then(function () {
-                            // newsReportProposal(finalId, status)
+                            verification(finalId)
                         });
                 }
             });
@@ -90,4 +193,58 @@ $('#final-status-table tbody').on('click', '.final-status-check', function () {
 // Agreement
 $('#final-project-table tbody').on('click', '.progress-agreement', function () {
     $('#final-progress-agreement-modal').modal('show')
+})
+
+
+// Store
+$(document).on('submit', '#final-project-active-form', function (e) {
+    e.preventDefault();
+
+    let id = $('#final-project-active-id').val();
+    let url = "../data/final_project/" + id;
+
+    let formData = new FormData(this);
+
+    formData.append("_method", "PUT");
+
+    if (url !== '') {
+        Swal.fire({
+            title: 'Loading',
+            timer: 60000,
+            onBeforeOpen: () => {
+                Swal.showLoading()
+            }
+        });
+
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                $('#final-project-active-form')[0].reset();
+
+                if (data !== "Failed") {
+                    Swal.fire({
+                            type: 'success',
+                            title: 'Berhasil!',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                        .then(function () {
+                            $('#final-project-active-modal').modal('hide');
+                            dataTable.ajax.reload();
+                        });
+                } else {
+                    Swal.fire({
+                        type: 'error',
+                        title: 'Error',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            }
+        });
+    }
 })
