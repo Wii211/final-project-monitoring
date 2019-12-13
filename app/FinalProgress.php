ae0d748dff5a8b56590ceeaaa7b829a57c23af25
Irham <irhammaulani@gmail.com>
2019-12-13 21:09:54 +0800

    change relationship finalLog

--- app/FinalProgress.php
+++ app/FinalProgress.php
@@ -2,13 +2,14 @@
 
 namespace App;
 
+use App\FinalLog;
 use App\Supervisor;
 use Illuminate\Database\Eloquent\Model;
 
 class FinalProgress extends Model
 {
-    public function supervisor()
+    public function finalLog()
     {
-        return $this->belongsTo(Supervisor::class);
+        return $this->belongsTo(FinalLog::class);
     }
 }
