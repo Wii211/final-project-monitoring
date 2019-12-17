55b4f5f5104bd1125cfc4e7f576b66220441e9f7
addid10 <rezkynewaditya@gmail.com>
2019-12-17 13:19:01 +0800

    add table row

--- resources/views/final_projects/students.blade.php
+++ resources/views/final_projects/students.blade.php
@@ -17,7 +17,10 @@
                     <thead>
                         <tr>
                             <th>NIM</th>
+                            <th>Image</th>
                             <th>Nama</th>
+                            <th></th>
+                            <th></th>
                             <th>Status Verifikasi</th>
                         </tr>
                     </thead>
@@ -33,6 +36,7 @@
 @section('modal')
 @include('modals.final_project.progress')
 @include('modals.final_project.detail')
+@include('modals.student.information')
 @endsection
 
 
@@ -40,5 +44,6 @@
     <!-- DataTables -->
     <script type="text/javascript" src="{{ asset('assets/plugins/datatables/jquery.dataTables.js') }}"></script>
     <script type="text/javascript" src="{{ asset('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/pdfobject.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('assets/js/final_project/student.js') }}"></script>
 @endsection
