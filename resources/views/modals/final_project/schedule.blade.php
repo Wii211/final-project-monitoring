e2ece7883cbca698c7971d44fe9116b40f1e4de2
addid10 <rezkynewaditya@gmail.com>
2019-12-17 17:39:48 +0800

    fix input form schedule

--- resources/views/modals/final_project/schedule.blade.php
+++ resources/views/modals/final_project/schedule.blade.php
@@ -1,62 +1,63 @@
-<div class="modal fade" id="scheduleFinalProject" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Jadwal Seminar Proposal/Sidang TA</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form>
-                        <div class="form-group">
-                            <label for="exampleFormControlInput1">Tanggal</label>
-                            <input type="date" class="form-control" id="exampleFormControlInput1">
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleFormControlInput1">Waktu</label>
-                            <input type="time" class="form-control" id="exampleFormControlInput1">
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleFormControlInput1">Tempat Seminar</label>
-                            <input type="text" class="form-control" id="exampleFormControlInput1">
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleFormControlSelect1">Dosen Penguji Pertama</label>
-                            <select class="form-control" id="exampleFormControlSelect1">
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleFormControlSelect1">Dosen Penguji Kedua</label>
-                            <select class="form-control" id="exampleFormControlSelect1">
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleFormControlSelect1">Dosen Penguji Ketiga</label>
-                            <select class="form-control" id="exampleFormControlSelect1">
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </select>
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Ajukan</button>
-                </div>
+<div class="modal fade" id="final-schedule-modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="final-schedule-title"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="final-schedule-form">
+                    <div class="form-group">
+                        <label for="final-schedule-type">Jenis Jadwal Tugas Akhir</label>
+                        <select class="form-control form-control-sm" id="final-schedule-type">
+                            <option value="seminar">Seminar Proposal</option>
+                            <option value="sidang">Sidang Tugas Akhir</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="final-project-schedule-id">Judul Tugas Akhir</label>
+                        <select class="form-control form-control-sm" id="final-project-schedule-id">
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="date">Tanggal</label>
+                        <input type="date" class="form-control form-control-sm" id="date">
+                    </div>
+                    <div class="form-group">
+                        <label for="time">Waktu</label>
+                        <input type="time" class="form-control form-control-sm" id="time">
+                    </div>
+                    <div class="form-group">
+                        <label for="place">Tempat Seminar</label>
+                        <input type="text" class="form-control form-control-sm" id="place">
+                    </div>
+                    <div class="form-group">
+                        <label for="first-examiner-name">Dosen Penguji Pertama</label>
+                        <input type="hidden" name="examiner[role]" id="first-examiner-role">
+                        <select class="form-control form-control-sm" name="examiner[lecturer_id]" id="first-examiner-name">
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="second-examiner-name">Dosen Penguji Kedua</label>
+                        <input type="hidden" name="examiner[role]" id="second-examiner-role">
+                        <select class="form-control form-control-sm" name="examiner[lecturer_id]" id="second-examiner-name">
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="third-examiner-name">Dosen Penguji Ketiga</label>
+                        <input type="hidden" name="examiner[role]" id="third-examiner-role">
+                        <select class="form-control form-control-sm" name="examiner[lecturer_id]" id="third-examiner-name">
+                        </select>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <input type="hidden" id="final-schedule-id">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" id="final-schedule-button">Ajukan</button>
             </div>
         </div>
     </div>
+</div>
