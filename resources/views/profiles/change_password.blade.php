92bf2e9782747656c699f198f0f4e071f4d0ce93
addid10 <rezkynewaditya@gmail.com>
2019-12-20 02:27:52 +0800

    add ucdraw design in change password page

--- resources/views/profiles/change_password.blade.php
+++ resources/views/profiles/change_password.blade.php
@@ -20,36 +20,46 @@
                 @endif
                 <form action="{{route('change_password.update', ['id' => Auth::user()->id]) }}" method="POST">
                     @csrf
-                    <div class="form-group row">
-                        <label class="col-3" for="oldPassword">Password Lama</label>
-                        <div class="col-9">
-                            <input type="password" class="form-control form-control-sm" id="oldPassword"
-                                name="old_password" required>
+                    <div class="row">
+                        <div class="col-md-9">
+
+                            <div class="form-group row">
+                                <label class="col-3" for="oldPassword">Password Lama</label>
+                                <div class="col-9">
+                                    <input type="password" class="form-control form-control-sm" id="oldPassword"
+                                        name="old_password" required>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="form-group row">
+                                <label class="col-3" for="password">Password Baru</label>
+                                <div class="col-9">
+                                    <input type="password" class="form-control form-control-sm" id="password"
+                                        name="password" minLength="8" maxlength="16" required>
+                                    <div id="passwordStatus"></div>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-3" for="exampleFormControlInput1">Konfirmasi Password Baru</label>
+                                <div class="col-9">
+                                    <small class="form-text">Panjang kata sandi harus 8-16 karakter.</small>
+                                    <input type="password" class="form-control form-control-sm" id="confirmPassword"
+                                        minLength="8" maxlength="16" name="confirm_password" required>
+                                    <div id="confirmStatus"></div>
+                                </div>
+                            </div>
+                            <input type="hidden" name="_method" value="PUT">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="{{ asset('storage/design/undraw_my_password_d6kg.png') }}" class="w-100" srcset="">
+                        </div>
+                        <div class="col-md-12">
+                            <button type="submit" class="btn btn-primary mb-2 w-100" data-toggle="modal"
+                                data-target="#pengajuanProposal">
+                                <i class="fas fa-key mr-2"></i>
+                                Ganti Password</button>
                         </div>
                     </div>
-                    <hr>
-                    <div class="form-group row">
-                        <label class="col-3" for="password">Password Baru</label>
-                        <div class="col-9">
-                            <input type="password" class="form-control form-control-sm" id="password" name="password"
-                                minLength="8" maxlength="16" required>
-                            <div id="passwordStatus"></div>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-3" for="exampleFormControlInput1">Konfirmasi Password Baru</label>
-                        <div class="col-9">
-                            <small class="form-text">Panjang kata sandi harus 8-16 karakter.</small>
-                            <input type="password" class="form-control form-control-sm" id="confirmPassword"
-                                minLength="8" maxlength="16" name="confirm_password" required>
-                            <div id="confirmStatus"></div>
-                        </div>
-                    </div>
-                    <input type="hidden" name="_method" value="PUT">
-                    <button type="submit" class="btn btn-primary mb-2 w-100" data-toggle="modal"
-                        data-target="#pengajuanProposal">
-                        <i class="fas fa-key mr-2"></i>
-                        Ganti Password</button>
                 </form>
             </div>
         </div>
