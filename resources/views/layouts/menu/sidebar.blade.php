aeab94c8191d06a79789bfb0b5032329cdb2f5d7
addid10 <rezkynewaditya@gmail.com>
2019-12-19 16:20:52 +0800

    conditional if user doesn't have image_profile

--- resources/views/layouts/menu/sidebar.blade.php
+++ resources/views/layouts/menu/sidebar.blade.php
@@ -1,8 +1,9 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <a href="index3.html" class="brand-link">
-          {{-- <img src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-              style="opacity: .8">
+          {{-- <img src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image
+          img-circle elevation-3"
+          style="opacity: .8">
           <span class="brand-text font-weight-light">TEMON </span> --}}
       </a>
 
@@ -10,15 +11,21 @@
       <div class="sidebar">
           <div class="user-panel mt-3 pb-3 mb-3 d-flex">
               <div class="image">
-                  <img src="{{ asset('storage/'.Auth::user()->image_profile) }}" class="img-circle elevation-2" alt="User Image">
+                  @if(Auth::user()->image_profile !== null)
+                  <img src="{{ asset('storage/'.Auth::user()->image_profile) }}" class="img-circle elevation-2"
+                      alt="User Image">
+                  @else
+                  <img src="{{ asset('storage/image_profile/default.png') }}" class="img-circle elevation-2"
+                      alt="User Image">
+                  @endif
               </div>
               <div class="info">
-                    @if(Auth::user()->finalStudent !== null)
-                    <div class="text-white d-block">{{ Auth::user()->finalStudent->name }}</div>
-                    <div class="text-white d-block text-NIM">{{ Auth::user()->finalStudent->student_id }}</div>
-                    @else
-                    <div class="text-white d-block">{{ Auth::user()->user_name }}</div>
-                    @endif
+                  @if(Auth::user()->finalStudent !== null)
+                  <div class="text-white d-block">{{ Auth::user()->finalStudent->name }}</div>
+                  <div class="text-white d-block text-NIM">{{ Auth::user()->finalStudent->student_id }}</div>
+                  @else
+                  <div class="text-white d-block">{{ Auth::user()->user_name }}</div>
+                  @endif
               </div>
           </div>
 
@@ -28,29 +35,29 @@
                   data-accordion="false">
                   @if(Auth::user()->isStudent())
                   <li class="nav-item">
-                      <a href="{{ route('final_registration.index') }}" 
-                        class="nav-link {{ Request::is('student') ? 'active' : '' }}">
+                      <a href="{{ route('final_registration.index') }}"
+                          class="nav-link {{ Request::is('student') ? 'active' : '' }}">
                           <i class="nav-icon fas fa-tachometer-alt"></i>
                           <p>
-                            Home
+                              Home
                           </p>
                       </a>
                   </li>
                   <li class="nav-item">
-                      <a href="{{ route('pre_proposal.index') }}" 
-                      class="nav-link {{ Request::is('student/pre_proposal') ? 'active' : '' }}">
+                      <a href="{{ route('pre_proposal.index') }}"
+                          class="nav-link {{ Request::is('student/pre_proposal') ? 'active' : '' }}">
                           <i class="nav-icon fas fa-sticky-note"></i>
                           <p>
-                            Pra-Proposal
+                              Pra-Proposal
                           </p>
                       </a>
                   </li>
                   <li class="nav-item">
-                      <a id="studentFinalProject" href="{{ route('final_project.index') }}" 
-                      class="nav-link {{ Request::is('student/final_project') ? 'active' : '' }}">
+                      <a id="studentFinalProject" href="{{ route('final_project.index') }}"
+                          class="nav-link {{ Request::is('student/final_project') ? 'active' : '' }}">
                           <i class="nav-icon fas fa-copy"></i>
                           <p>
-                            Proposal/Tugas Akhir
+                              Proposal/Tugas Akhir
                           </p>
                       </a>
                   </li>
@@ -58,50 +65,50 @@
                   @if(Auth::user()->isCoordinator())
                   <li class="nav-item">
                       <a id="coorHome" href="{{ route('coordinator_dashboard.index') }}"
-                      class="nav-link {{ Request::is('coordinator') ? 'active' : '' }}">
+                          class="nav-link {{ Request::is('coordinator') ? 'active' : '' }}">
                           <i class="nav-icon fas fa-home"></i>
                           <p>
-                            Home
+                              Home
                           </p>
                       </a>
                   </li>
                   @endif
                   <li class="nav-item">
-                      <a id="coorFinalProjectStudent" href="{{ route('recomendation-title.index') }}" 
-                      class="nav-link {{ Request::is('recomendation-title') ? 'active' : '' }}">
+                      <a id="coorFinalProjectStudent" href="{{ route('recomendation-title.index') }}"
+                          class="nav-link {{ Request::is('recomendation-title') ? 'active' : '' }}">
                           <i class="nav-icon fas fa-user-graduate"></i>
                           <p>
-                            Rekomendasi Topik/Judul
+                              Rekomendasi Topik/Judul
                           </p>
                       </a>
                   </li>
                   @if(Auth::user()->isCoordinator())
                   <li class="nav-item">
-                      <a id="coorFinalProjectStudent" href="{{ route('final_students_verify.index') }}" 
-                      class="nav-link {{ Request::is('coordinator/final_projects/students*') ? 'active' : '' }}">
+                      <a id="coorFinalProjectStudent" href="{{ route('final_students_verify.index') }}"
+                          class="nav-link {{ Request::is('coordinator/final_projects/students*') ? 'active' : '' }}">
                           <i class="nav-icon fas fa-user-graduate"></i>
                           <p>
-                            Mahasiswa TA
-                            <span class="badge badge-danger right">2</span>
+                              Mahasiswa TA
+                              <span class="badge badge-danger right">2</span>
                           </p>
                       </a>
                   </li>
                   <li class="nav-item">
                       <a id="coorFinalProject" href="{{ route('final_actives.index') }}"
-                      class="nav-link {{ Request::is('coordinator/final_projects') ? 'active' : '' }}">
+                          class="nav-link {{ Request::is('coordinator/final_projects') ? 'active' : '' }}">
                           <i class="nav-icon fas fa-book"></i>
                           <p>
-                            Tugas Akhir
-                            <span class="badge badge-danger right">2</span>
+                              Tugas Akhir
+                              <span class="badge badge-danger right">2</span>
                           </p>
                       </a>
                   </li>
                   <li class="nav-item">
                       <a id="coorFinalProjectSchedule" href="{{ route('final_schedules.index') }}"
-                      class="nav-link {{ Request::is('coordinator/final_projects/schedules') ? 'active' : '' }}">
+                          class="nav-link {{ Request::is('coordinator/final_projects/schedules') ? 'active' : '' }}">
                           <i class="nav-icon fas fa-calendar-alt"></i>
                           <p>
-                            Seminar Proposal/Sidang TA
+                              Seminar Proposal/Sidang TA
                           </p>
                       </a>
                   </li>
@@ -110,25 +117,25 @@
                   <li class="nav-header">DATA</li>
                   <li class="nav-item">
                       <a id="finalProject" href="{{ route('final_projects.index') }}"
-                      class="nav-link {{ Request::is('data/final_projects') ? 'active' : '' }}">
+                          class="nav-link {{ Request::is('data/final_projects') ? 'active' : '' }}">
                           <i class="nav-icon fas fa-copy"></i>
                           <p>
-                            Data Tugas Akhir
+                              Data Tugas Akhir
                           </p>
                       </a>
                   </li>
                   <li class="nav-item">
-                      <a id="lecturer" href="{{ route('lecturers.index') }}" 
-                      class="nav-link {{ Request::is('data/lecturers') ? 'active' : '' }}">
+                      <a id="lecturer" href="{{ route('lecturers.index') }}"
+                          class="nav-link {{ Request::is('data/lecturers') ? 'active' : '' }}">
                           <i class="nav-icon fas fa-copy"></i>
                           <p>
-                            Data Dosen
+                              Data Dosen
                           </p>
                       </a>
                   </li>
                   <li class="nav-item">
-                      <a id="student" href="{{ route('students.index') }}" 
-                      class="nav-link {{ Request::is('data/students') ? 'active' : '' }}">
+                      <a id="student" href="{{ route('students.index') }}"
+                          class="nav-link {{ Request::is('data/students') ? 'active' : '' }}">
                           <i class="nav-icon fas fa-users"></i>
                           <p>
                               Data Mahasiswa
@@ -139,7 +146,7 @@
                   <li class="nav-header">PROFILE</li>
                   <li class="nav-item">
                       <a id="changeProfile" href="{{ route('change_profile.index') }}"
-                            class="nav-link {{ Request::is('profile/change_profile') ? 'active' : '' }}">
+                          class="nav-link {{ Request::is('profile/change_profile') ? 'active' : '' }}">
                           <i class="nav-icon fas fa-key"></i>
                           <p>
                               Profil Saya
@@ -148,7 +155,7 @@
                   </li>
                   <li class="nav-item">
                       <a id="changePassword" href="{{ route('change_password.index') }}"
-                      class="nav-link {{ Request::is('profile/change_password') ? 'active' : '' }}">
+                          class="nav-link {{ Request::is('profile/change_password') ? 'active' : '' }}">
                           <i class="nav-icon fas fa-key"></i>
                           <p>
                               Ganti Password
