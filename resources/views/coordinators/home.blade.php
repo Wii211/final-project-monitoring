8d85a67cd0f2c83de6d97843c9b5cf3d5f5145bb
addid10 <rezkynewaditya@gmail.com>
2019-12-19 18:40:12 +0800

    fix design for deadline settings

--- resources/views/coordinators/home.blade.php
+++ resources/views/coordinators/home.blade.php
@@ -17,16 +17,31 @@
                 </div>
                 <form action="{{route('dead-line.store')}}" method="POST">
                     @csrf
-                    <div class="form-group row">
-                        <label class="col-md-2" for="exampleFormControlInput1">Pendaftaran dimulai</label>
-                        <div class="col-md-4">
-                            <input type="date" class="form-control form-control-sm" name="registration_start_date"
-                                value="{{ date('Y-m-d', strtotime($deadlineSchedule[0]->start_date)) }}">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="{{ asset('storage/design/undraw_timeline_9u4u.png') }}" class="w-100" srcset="">
                         </div>
-                        <label class="col-md-2" for="exampleFormControlInput1">Deadline Pendaftaran</label>
-                        <div class="col-md-4">
-                            <input type="date" class="form-control form-control-sm" name="registration_end_date"
-                                value="{{ date('Y-m-d', strtotime($deadlineSchedule[0]->end_date)) }}">
+                        <div class="col-md-9">
+                            <div class="form-group row">
+                                <label class="col-md-4" for="exampleFormControlInput1">Pendaftaran dimulai</label>
+                                <div class="col-md-8">
+                                    <input type="date" class="form-control form-control-sm"
+                                        name="registration_start_date"
+                                        value="{{ date('Y-m-d', strtotime($deadlineSchedule[0]->start_date)) }}">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-md-4" for="exampleFormControlInput1">Deadline Pendaftaran</label>
+                                <div class="col-md-8">
+                                    <input type="date" class="form-control form-control-sm" name="registration_end_date"
+                                        value="{{ date('Y-m-d', strtotime($deadlineSchedule[0]->end_date)) }}">
+                                </div>
+                            </div>
+                            <hr>
+                            <blockquote>
+                                <i class="fas fa-info-circle" aria-hidden="true"></i> Deadline untuk mahasiswa melakukan
+                                pendaftaran proposal.
+                            </blockquote>
                         </div>
                     </div>
                     <hr>
@@ -34,14 +49,14 @@
                         <label class="col-md-2" for="exampleFormControlInput1">Deadline Pra-Proposal</label>
                         <div class="col-md-4">
                             <input type="date" class="form-control form-control-sm" name="pre_proposal_end_date"
-                                value="{{ date('Y-m-d', strtotime($deadlineSchedule[1]->end_date)) }}"> Hari
-                            setelah pendaftaran ditutup.
+                                value="{{ date('Y-m-d', strtotime($deadlineSchedule[1]->end_date)) }}"> Dimulai
+                            setelah deadline pendaftaran.
                         </div>
                         <label class="col-md-2" for="exampleFormControlInput1">Deadline Proposal</label>
                         <div class="col-md-4">
                             <input type="date" class="form-control form-control-sm" name="proposal_end_date"
-                                value="{{ date('Y-m-d', strtotime($deadlineSchedule[2]->end_date)) }}"> Hari
-                            setelah pra-proposal ditutup.
+                                value="{{ date('Y-m-d', strtotime($deadlineSchedule[2]->end_date)) }}"> Dimulai
+                            setelah deadline pra-proposal.
                         </div>
                     </div>
                     <div class="form-group row">
@@ -49,7 +64,7 @@
                         <div class="col-md-4">
                             <input type="date" class="form-control form-control-sm" name="revision_proposal_end_date"
                                 value="{{ date('Y-m-d', strtotime($deadlineSchedule[3]->end_date)) }}">
-                            Hari setelah proposal ditutup.
+                            Dimulai setelah deadline proposal.
                         </div>
                     </div>
                     <hr>
@@ -57,15 +72,15 @@
                         <label class="col-md-2" for="exampleFormControlInput1">Deadline Tugas Akhir</label>
                         <div class="col-md-4">
                             <input type="date" class="form-control form-control-sm" name="final_project_end_date"
-                                value="{{ date('Y-m-d', strtotime($deadlineSchedule[4]->end_date)) }}"> Hari
-                            setelah revisi proposal ditutup.
+                                value="{{ date('Y-m-d', strtotime($deadlineSchedule[4]->end_date)) }}"> Dimulai
+                            setelah deadline revisi proposal.
                         </div>
                         <label class="col-md-2" for="exampleFormControlInput1">Deadline Revisi Tugas Akhir</label>
                         <div class="col-md-4">
                             <input type="date" class="form-control form-control-sm"
                                 name="revision_final_project_end_date"
-                                value="{{ date('Y-m-d', strtotime($deadlineSchedule[5]->end_date)) }}"> Hari setelah
-                            tugas akhir ditutup.
+                                value="{{ date('Y-m-d', strtotime($deadlineSchedule[5]->end_date)) }}"> Dimulai setelah
+                            deadline tugas akhir.
                         </div>
                     </div>
                     <button type="submit" class="btn btn-primary w-100">Perbarui Deadline</button>
