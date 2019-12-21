85c4dbc00d6eac065eab66e5d849e99199af02d8
addid10 <rezkynewaditya@gmail.com>
2019-12-21 19:55:35 +0800

    change navbar color

--- resources/views/layouts/menu/menu.blade.php
+++ resources/views/layouts/menu/menu.blade.php
@@ -1,5 +1,5 @@
   <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+  <nav class="main-header navbar navbar-expand navbar-primary navbar-dark">
       <ul class="navbar-nav">
           <li class="nav-item">
               <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
@@ -9,8 +9,8 @@
           <li class="nav-item">
               <form action="{{ route('logout') }}" method="POST">
               @csrf
-              <button type="submit" class="nav-link" style="background: white; border:0px">
-                  <i class="fas fa-sign-out-alt"></i>
+              <button type="submit" class="nav-link btn-primary" style="background: white; border:0px">
+                  <i class="fas fa-sign-out-alt"></i> Logout
               </button>
               </form>
           </li>
