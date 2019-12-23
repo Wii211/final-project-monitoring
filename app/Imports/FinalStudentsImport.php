70ba565eb6142cbf3a8fd8a359e53a89e8560217
Irham <irhammaulani@gmail.com>
2019-12-23 15:34:09 +0800

    remove comment code

--- app/Imports/FinalStudentsImport.php
+++ app/Imports/FinalStudentsImport.php
@@ -17,15 +17,6 @@ class FinalStudentsImport implements ToCollection, WithHeadingRow
     public function collection(Collection $rows)
     {
         foreach ($rows as $row) {
-            // $user = new User([
-            //     'user_name' => $row['nim'],
-            // 'email' => $row['email'],
-            // 'phone_number' => $row['nomor_telepon'],
-            // 'gender' => $row['jenis_kelamin'],
-            // 'image_profile' => null,
-            // 'password' => $row['nim'],
-            // ]);
-
             $user  = User::updateOrCreate(
                 ['user_name' => $row['nim']],
                 [
@@ -37,18 +28,7 @@ class FinalStudentsImport implements ToCollection, WithHeadingRow
                 ]
             );
 
-
-
-            // $user->save();
             $user->roles()->sync(Role::whereName('mahasiswa')->first()->id);
-
-            // FinalStudent::create([
-            //     'student_id' => $row['nim'],
-            // 'status' => FinalStudent::convertActive($row['status']),
-            // 'is_verified' => 0,
-            // 'user_id' => $user->id,
-            // 'name' => $row['nama']
-            // ]);
 
             FinalStudent::updateOrCreate(
                 ['student_id' => $row['nim']],
