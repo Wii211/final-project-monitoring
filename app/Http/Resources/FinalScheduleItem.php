d3abd1f4e449360c453dcd38651b29aff1ebd5b9
Irham <irhammaulani@gmail.com>
2019-12-29 18:53:49 +0800

    add response in finalSchedule

---
+++ app/Http/Resources/FinalScheduleItem.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Examiner;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FinalScheduleItem extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'    => $this->id,
+            'student_name' => $this->finalLog->finalProject->finalStudent->name,
+            'final_log_id' => $this->final_log_id,
+            'title' => $this->finalLog->finalProject->title,
+            'place' => $this->place,
+            'date' => $this->date,
+            'hour' => $this->hour,
+            'examiners' => Examiner::filterExaminer($this->finalLog->final_status_id, $this->finalLog->finalProject->examiners)
+        ];
+    }
+}
