70e3a36667791c3a646c5ee66a93767bbeeced89
Irham <irhammaulani@gmail.com>
2019-12-30 18:29:33 +0800

    add alreadySubmitted function

--- app/FinalRequirement.php
+++ app/FinalRequirement.php
@@ -13,4 +13,14 @@ class FinalRequirement extends Model
     {
         return $this->belongsTo(FinalLog::class);
     }
+
+    public function alreadySubmitted($finalLogId)
+    {
+        try {
+            $finalRequirement  = $this->finalRequirement->findOrFail($finalLogId);
+            return $finalRequirement ? true : false;
+        } catch (\Throwable $th) {
+            return false;
+        }
+    }
 }
