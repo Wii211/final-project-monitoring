9cc665dd9b4f9861505ad195ccf3ed550c715fd8
Irham <irhammaulani@gmail.com>
2020-01-01 22:19:09 +0800

    add accept and decline for finalScheduleStatus

---
+++ app/Http/Controllers/FinalScheduleStatusController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\FinalSchedule;
+use Illuminate\Http\Request;
+
+class FinalScheduleStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //Accept finalSchedule
+        try {
+            $finalSchedule = FinalSchedule::whereFinalLogId($request->finalLogId);
+            $finalSchedule->status = 1;
+            $finalSchedule->save();
+        } catch (\Throwable $th) {
+            return response()->json("Failed");
+        }
+        return response()->json("Success");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, $id)
+    {
+        //Decline finalSchedule
+        //Accept finalSchedule
+        try {
+            $finalSchedule = FinalSchedule::whereFinalLogId($request->finalLogId);
+            $finalSchedule->status = 2;
+            $finalSchedule->save();
+        } catch (\Throwable $th) {
+            return response()->json("Failed");
+        }
+        return response()->json("Success");
+    }
+}
