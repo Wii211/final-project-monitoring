ac59c83c081322f6055408e432ff9562659565b0
Irham <irhammaulani@gmail.com>
2020-01-04 23:42:36 +0800

    add formatDateWithDay

--- app/Helpers/DateHelper.php
+++ app/Helpers/DateHelper.php
@@ -19,6 +19,13 @@ class DateHelper
         return $date->formatLocalized('%d %B %Y');
     }
 
+    public function formatDateWithDayName($date)
+    {
+        setlocale(LC_TIME, 'id_ID.utf8');
+
+        return $date->formatLocalized('%A %d %B %Y');
+    }
+
     public static function convertTimeHour($time)
     {
         return date('H:i', strtotime($time));
