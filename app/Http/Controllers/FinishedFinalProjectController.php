1fc18e44fec2a7cf3ade8d05dbbd47e322d7469e
Irham <irhammaulani@gmail.com>
2020-01-04 13:58:44 +0800

    add final_project completed

--- app/Http/Controllers/FinishedFinalProjectController.php
+++ app/Http/Controllers/FinishedFinalProjectController.php
@@ -18,18 +18,27 @@ class FinishedFinalProjectController extends Controller
         //ready for presents Final Project
 
         $status = 'tugas_akhir';
+
         $verification = 1;
-        if ($request->has('status')) $status = $request->query('status');
-        if ($request->has('verification')) $verification = $request->query('verification');
+
+        if ($request->has('status'))
+            $status = $request->query('status');
+
+        if ($request->has('verification')) $verification =
+            $request->query('verification');
 
 
-        $finalProject = FinalProject::whereHas('finalLogs', function ($query) use ($status, $verification) {
-            $query->whereHas('finalRequirements', function ($query) {
-            });
+        $finalProject = FinalProject::whereHas(
+            'finalLogs',
+            function ($query) use ($status, $verification) {
+                $query->whereHas('finalRequirements', function ($query) {
+                });
+                $query->whereFinalStatusId(FinalStatus::name($status));
+                $query->whereIsVerification($verification);
+            }
+        )->with(['finalLogs' => function ($query) use ($status) {
             $query->whereFinalStatusId(FinalStatus::name($status));
-            $query->whereIsVerification($verification);
-        })->with(['finalLogs' => function ($query) use ($status) {
-            $query->whereFinalStatusId(FinalStatus::name($status));
+            $query->where('final_status_id', '!=', FinalStatus::name('tugas_akhir_selesai'));
             $query->whereHas('finalRequirements');
             $query->with('finalRequirements');
         }, 'finalStudent'])
