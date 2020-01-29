f4324352d85a3d38d6dc76a2d777d32cc04c089a
addid10 <rezkynewaditya@gmail.com>
2020-01-29 12:06:41 +0800

    add quota for lecturer design

--- public/assets/js/lecturer/lecturer.js
+++ public/assets/js/lecturer/lecturer.js
@@ -9,7 +9,7 @@ let dataTable = $('#lecturerTable').DataTable({
     "ajax": {
         // dataSrc: ""
     },
-    "order": [[ 4, "asc" ]],
+    "order": [[ 6, "asc" ]],
     "columns": [{
             data: 'personnel_id'
         },
@@ -32,10 +32,10 @@ let dataTable = $('#lecturerTable').DataTable({
             data: 'name'
         },
         {
-            sortable: false,
-            "render": function (data, type, full, meta) {
-                return "0";
-            }
+            data: 'primary_supervisors_count'
+        },
+        {
+            data: 'secondary_supervisors_count'
         },
         {
             data: 'status'
@@ -63,7 +63,7 @@ let dataTable = $('#lecturerTable').DataTable({
         }
     ],
     "columnDefs": [{
-        targets: [5],
+        targets: [6],
         render: function (data, type, row) {
             if (data == 1) {
                 return '<span class="badge badge-success p-2">Aktif</span>';
