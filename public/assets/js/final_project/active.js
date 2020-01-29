a7b4a29405a8c22e301ac10110e447b434d78337
addid10 <rezkynewaditya@gmail.com>
2020-01-29 12:07:52 +0800

    fix

--- public/assets/js/final_project/active.js
+++ public/assets/js/final_project/active.js
@@ -36,7 +36,7 @@ let dataTable = $('#final-project-table').DataTable({
     "ajax": {
         url: "monitoring"
     },
-    "order": [[ 2, "asc" ]],
+    "order": [[ 2, "desc" ]],
     "columns": [{
             data: 'name'
         },
